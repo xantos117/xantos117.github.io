29b0393870c6cc4be1046f781aab58432c16f6c6
Theodore Lincoln <tlincol2@u.rochester.edu>
2021-08-30 20:36:02 -0400

    Changed loading of anim to module style. Added dat.gui module. Added gui element to toggle animation.

--- docs/js/anim.js
+++ docs/js/anim.js
@@ -1,9 +1,12 @@
+import * as dat from "../lib/dat.gui.module.js";
+
 // Our Javascript will go here.
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
+const gui = new dat.gui.GUI();
 scene.add( cube );
 
 camera.position.z = 5;
@@ -13,19 +16,25 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 
-var anim = false;
+//var anim = false;
 
-window.addEventListener('keydown', (e) => {
-    if(!e.repeat) {
-        console.log('Key "${e.key}" pressed  [event: keydown]');
-        anim = !anim;
-    }
-    else {
-        console.log('Key "${e.key} is held down.');
-    }
-})
+let doesAnimate = {
+    value: true,
+  };
+
+gui.add(doesAnimate, "value").name("Move with time?");
+
+// window.addEventListener('keydown', (e) => {
+//     if(!e.repeat) {
+//         console.log('Key "${e.key}" pressed  [event: keydown]');
+//         anim = !anim;
+//     }
+//     else {
+//         console.log('Key "${e.key} is held down.');
+//     }
+// })
 function animate() {
-    if(anim) {
+    if(doesAnimate.value) {
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;	
         //camera.rotation.x += 0.01;
