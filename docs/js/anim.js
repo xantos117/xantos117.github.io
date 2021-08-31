45af7b2ddca0d84186d98adc027f3ab9528ae6e2
Theodore Lincoln <tlincol2@u.rochester.edu>
2021-08-31 17:02:55 -0400

    added color, light and a sphere

--- docs/js/anim.js
+++ docs/js/anim.js
@@ -2,14 +2,26 @@ import * as dat from "../lib/dat.gui.module.js";
 
 // Our Javascript will go here.
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 2000 );
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const geometry2 = new THREE.SphereGeometry();
+const material = new THREE.MeshPhongMaterial({shininess: 10, color: 0x5F9EA0});
 const cube = new THREE.Mesh( geometry, material );
+const sphere = new THREE.Mesh(geometry2, material );
+const light1 = new THREE.AmbientLight({hex: 0xffffff, intensity: 0.25});
+const light2 = new THREE.PointLight(0xffffff, 0.5);
 const gui = new dat.gui.GUI();
 scene.add( cube );
+scene.add(sphere);
+scene.add( light1 );
+scene.add( light2 );
 
-camera.position.z = 5;
+camera.position.z = 20;
+
+light2.position.x = 5;
+light2.position.y = 5;
+light2.position.z = 5;
+sphere.position.x = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
