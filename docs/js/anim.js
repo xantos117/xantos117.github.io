67c3e0b4f894b77d27bf6603a4d2eca397657607
Theodore Lincoln <tlincol2@u.rochester.edu>
2021-09-11 15:41:04 -0400

    Got time difference functionality working. Set animation to scale based on elapsed time. Added 2 buttons to replace animation toggle, Animate and Reset. Adjusted animations to fit assignment.

--- docs/js/anim.js
+++ docs/js/anim.js
@@ -7,21 +7,28 @@ const geometry = new THREE.BoxGeometry();
 const geometry2 = new THREE.SphereGeometry();
 const material = new THREE.MeshPhongMaterial({shininess: 10, color: 0x5F9EA0});
 const cube = new THREE.Mesh( geometry, material );
-const sphere = new THREE.Mesh(geometry2, material );
+//const sphere = new THREE.Mesh(geometry2, material );
 const light1 = new THREE.AmbientLight({hex: 0xffffff, intensity: 0.25});
 const light2 = new THREE.PointLight(0xffffff, 0.5);
+let d = new Date();
 const gui = new dat.gui.GUI();
+let initTime = 0;
+cube.scale.x = 10;
+cube.scale.y = 10;
+cube.scale.z = 10;
 scene.add( cube );
-scene.add(sphere);
+//scene.add(sphere);
 scene.add( light1 );
 scene.add( light2 );
 
-camera.position.z = 20;
+camera.position.z = 250;
+camera.position.x = 50;
+camera.position.y = 50;
 
 light2.position.x = 5;
 light2.position.y = 5;
 light2.position.z = 5;
-sphere.position.x = 5;
+//sphere.position.x = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -31,10 +38,21 @@ document.body.appendChild( renderer.domElement );
 //var anim = false;
 
 let doesAnimate = {
-    value: true,
+    value: false,
   };
+let dt = 0;
 
-gui.add(doesAnimate, "value").name("Move with time?");
+// let animToggle = gui.add(doesAnimate, "value").name("Move with time?").listen();
+
+// animToggle.onChange(function(){ initTime = Date.now(); });
+
+let anim_button = {Animate:function(){
+    initTime = Date.now();
+    doesAnimate.value = true;
+    dt = 0;
+}};
+
+gui.add(anim_button,'Animate');
 
 // window.addEventListener('keydown', (e) => {
 //     if(!e.repeat) {
@@ -45,15 +63,25 @@ gui.add(doesAnimate, "value").name("Move with time?");
 //         console.log('Key "${e.key} is held down.');
 //     }
 // })
+function deg_to_rad(degrees) {
+    return degrees * (Math.PI/180);
+}
+
+
 function animate() {
-    if(doesAnimate.value) {
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;	
+    if(doesAnimate.value && (dt/1000 <= 20)) {
+        dt = Date.now() - initTime;
+        //cube.rotation.x += 0.01;
+        cube.rotation.y = deg_to_rad(18*(dt/1000));	
+        cube.position.x = 5*(dt/1000);
+        cube.position.y = 5*(dt/1000);
         //camera.rotation.x += 0.01;
+        //console.log(initTime);
+        console.log(dt.toString());
     }
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
 }
-
+//dt = d.getTime();
 animate();
 
